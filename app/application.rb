4c21157202fb4184edd07382f965923be78bfc18
Steven Nunez <steven.nunez@gmail.com>
2017-09-26 19:30:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,6 +20,7 @@ class Application
         resp.write(@@cart.join("\n"))
       end
     elsif req.path == "/add"
+      @@items.push(req.params['item'])
       resp.write("added #{req.params['item']}")
     else
       resp.write "Path Not Found"
